d9ec0b07589c7b178e549114d4e54699929c3c31
Hina <hinakumari.725394@gmail.com>
2026-02-08 21:14:05 +0530

    initial commit

---
+++ quotegenerator/src/main/java/com/example/quotegenerator/QuotegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotegeneratorApplication.class, args);
+	}
+
+}
