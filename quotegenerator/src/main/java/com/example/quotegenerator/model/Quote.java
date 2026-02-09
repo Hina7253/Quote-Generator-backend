6e93864bd29f516e58128b3bd5d1f029e859d94a
Hina <hinakumari.725394@gmail.com>
2026-02-09 09:33:15 +0530

    add model class

--- quotegenerator/src/main/java/com/example/quotegenerator/model/Quote.java
+++ quotegenerator/src/main/java/com/example/quotegenerator/model/Quote.java
@@ -16,4 +16,45 @@ public class Quote {
         this.category = category;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", author='" + author + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
