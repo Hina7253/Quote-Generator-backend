2fc1b208c4896917978f55cb846428f0f36b29c5
Hina <hinakumari.725394@gmail.com>
2026-02-12 22:51:21 +0530

    Update QuoteController.java

--- quotegenerator/src/main/java/com/example/quotegenerator/controller/QuoteController.java
+++ quotegenerator/src/main/java/com/example/quotegenerator/controller/QuoteController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RestController
 @RequestMapping("/api/quotes")
 @CrossOrigin(origins = "*")
 public class QuoteController {
